073d30ebde89108466efc0665aed2b2e425bb7e1
benCodeur <benedictobed20@gmail.com>
2024-01-18 00:34:12 +0000

    project structure and routing done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-import './App.css'
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Cart from './pages/Cart';
+import Home from './pages/Home';
+import Checkout from './pages/Checkout';
+import Login from './pages/Login';
+import SignUp from './pages/Signup';
+import Shop from './pages/Shop';
+import ProductDetails from './pages/ProductDetails';
 
-const  App = () => {
-
-  return (
-    <>
-     <h2>Holaaa, Welcome to ECOBUD✨</h2>
-    </>
-  )
+ function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route path="/" element={<Home />} />
+        <Route exact path="signup" element={<SignUp />} />
+        <Route exact path="shop" element={<Shop />} />
+        <Route exact path="checkout" element={<Checkout />} />
+        <Route exact path="login" element={<Login />} />
+        <Route exact path="cart" element={<Cart />} />
+        <Route exact path="shop/:id" element={<ProductDetails />} />
+      </>
+    )
+  );
+  return <RouterProvider router={router} />;
 }
 
-export default App
+
+export default App;
