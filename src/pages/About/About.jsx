0cebe6cc849ff36d2315148c3165660ac79b90f8
Derek Addo <mr.derek.addo@gmail.com>
2024-01-31 20:45:06 +0000

    added about content on about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -3,9 +3,43 @@
 const About = ()=> {
     
     return (
-        <>
-        
-        </>
+      <>
+        <div>
+          <h2>Our Story: From Idea to Action</h2>
+          <p>
+            Welcome to Ecobuddi, where we believe in the power of
+            transformation. Our journey began with a simple yet profound idea -
+            to turn clutter into cash, and in the process, change the way
+            students interact with their unwanted belongings and the
+            environment.{" "}
+          </p>
+          <h2>The Birth of Ecobuddi</h2>
+          <p>
+            In the midst of brainstorming sessions and countless cups of coffee,
+            the Ecobuddi concept was born. We asked ourselves, "What if we could
+            create a marketplace that not only helped students declutter their
+            lives but also put money in their pockets?" That spark of
+            inspiration fueled our commitment to make it a reality.
+          </p>
+          <h2>Our Mission</h2>
+          <p>
+            We're not just a marketplace; we're a community that believes in the
+            potential of every item, the value of sustainability, and the
+            importance of student empowerment
+          </p>
+
+          <p>
+            Our team, hailing from multiple African countries, shares a deep
+            passion for technology and a commitment to making a positive impact
+            on the continent.
+          </p>
+          <p>
+            We invite you to join us on this exciting journey of solving a real
+            problem that impacts students and the environment. Together, we can
+            make a difference.
+          </p>
+        </div>
+      </>
     );
 };
 
