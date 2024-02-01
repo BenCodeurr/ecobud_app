96075e208c0d2a36c0770db2f67e8272c459280e
boypaida12 <baddysackey5@gmail.com>
2024-02-01 20:36:20 +0000

    merged ecobud group photo

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -45,7 +45,7 @@ const About = () => {
           }
         </p>
         <Button
-          className=" bg-orange-500 w-fit py-5 px-4 text-xl mx-auto text-white transition-all delay-[25ms] text-center rounded-xl font-bold"
+          className=" bg-orange-500 w-fit py-3 px-4 mx-auto text-white transition-all delay-[25ms] text-center rounded-xl font-bold"
           onClick={() => navigate("/shop")}
         >
           SHOP NOW
@@ -67,11 +67,11 @@ const About = () => {
           >
             {text.map((text, index)=> (
               <>
-              <SwiperSlide key={index} className="px-5 xl:px-20">
-                <h2 className=" text-primary text-4xl font-bold">
+              <SwiperSlide key={index} className="px-5 xl:px-16 text-center lg:text-left">
+                <h2 className=" text-primary xl:text-6xl text-4xl font-bold">
                   {text.title}
                 </h2>
-                <p className=" lg: mt-5">{text.content}</p>
+                <p className=" lg:mt-5 leading-6">{text.content}</p>
               </SwiperSlide>
             </>
           ))}
